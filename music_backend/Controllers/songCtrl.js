a9d1c24d8d6822a8a47f3ae3a117ce2145bb1dc4
kusumag004 <kusumag004@gmail.com>
2024-04-25 00:08:55 +0530

    check

---
+++ music_backend/Controllers/songCtrl.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const passport = require("passport");
+const Song = require("../models/Song");
+const User = require("../models/User");
+const createCtrl = async (req,res)=>{
+    const {name, thumbnail, track} = req.body;
+    if(!name || !thumbnail || !track) {
+        return res
+             .status(301)
+             .json({err: "Insufficient details to create song."});
+    }
+    //console.log(req.user?._id);
+    const artist = req.user._id;
+    const songDetails = {name, thumbnail, track, artist};
+    
+    const createdSong = await Song.create(songDetails);
+    return res.status(200).json(createdSong);
+}
+
+const getSongsCtrl = async (req,res)=>{
+    const currentUser = req.user;
+    const songs = await Song.find({artist: req.user._id}).populate("artist");
+    return res.status(200).json({data: songs});
+}
+
+const getArtistCtrl = async (req,res)=>{
+    const {artistId} = req.params;
+
+    const artist = await User.findOne({_id: artistId});
+    console.log(artist);
+    if(!artist){
+        return res.status(301).json({err: "Artist does not exist"});
+    } 
+
+    const songs = await Song.find({artist: artistId});
+    return res.status(200).json({data: songs});
+}
+
+
+const songNameCtrl = async (req,res)=>{
+    const {songName} = req.body;
+
+    const songs = await Song.find({name: songName});
+    return res.status(200).json({data: songs});
+}
+module.exports ={
+    createCtrl,
+    getSongsCtrl,
+    getArtistCtrl,
+    songNameCtrl
+};
