84944b411c931da916f564ed630dd14e8ac00e41
hema1mathi <hemasantoshimathi@gmail.com>
2024-04-26 01:24:39 +0530

    Add playlist to Library

--- frontend/src/Routes/Library.js
+++ frontend/src/Routes/Library.js
@@ -1,6 +1,49 @@
+import {useState, useEffect} from "react";
+import LoggedInContainer from "../Containers/LoggedInContainer";
+import { makeAuthenticatedGETRequest } from "../Util/serverHelper";
+
 const Library = () => {
-    return <h1>I am here</h1>;
+
+    const [myPlaylists, setMyPlaylists] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await makeAuthenticatedGETRequest(
+                "/playlist/get/me"
+            );
+            setMyPlaylists(response.data);
+        };
+        getData();
+    },[]);
+
+    return (
+        <LoggedInContainer curActiveScreen={"library"}>
+            <div className="text-white text-xl pt-8">My Playlists</div>
+            <div className="py-5 grid gap-5 grid-cols-5">
+                {myPlaylists.map(item => {
+                    return <Card key ={JSON.stringify(item)} title = {item.name} description="" imgUrl = {item.thumbnail}/>
+                })}
+            </div>
+        </LoggedInContainer>
+    );
 };
+
+const Card = ({title, description, imgUrl}) => {
+    return(
+        <div className="bg-black bg-opacity-40 w-full p-4 rounded-lg cursor-pointer">
+            <div className="pb-4 pt-2">
+                <img 
+                    className="w-full rounded-md" 
+                    src={imgUrl}
+                    alt="pic"
+                />
+            </div>
+            <div className="text-white font-semibold py-3">{title}</div>
+            <div className="text-gray-500 text-sm">{description}</div>
+        </div>
+    );
+};
+
 
 
 export default Library
--- music_backend/routes/playlistRoutes.js
+++ music_backend/routes/playlistRoutes.js
@@ -5,5 +5,6 @@ const {createCtrl,playlistIdCtrl, artistIdCtrl, addSongCtrl} = require("../Contr
 playlistRoute.post("/create",passport.authenticate("jwt", {session: false}),createCtrl);
 playlistRoute.get("/get/playlist/:playlistId",passport.authenticate("jwt", {session: false}),playlistIdCtrl);
 playlistRoute.get("/get/artist/artistId", passport.authenticate("jwt", {session: false}),artistIdCtrl);
+playlistRoute.get("/get/me", passport.authenticate("jwt", {session: false}),artistIdCtrl);
 playlistRoute.post("/add/song", passport.authenticate("jwt", {session: false}), addSongCtrl);
 module.exports = playlistRoute;
